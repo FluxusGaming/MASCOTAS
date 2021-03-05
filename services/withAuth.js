4748d6b78e0f9e5ee71f46be5dfd5d008cc51d26
FluxusGaming <agustint@live.com>
2021-03-05 15:17:23 -0300

    Chore: Auth fix

--- services/withAuth.js
+++ services/withAuth.js
@@ -8,7 +8,7 @@ const withAuth = (Component) => {
     if (isLoading) return <Loading />;
 
     // If user is not logged in, return login component
-    if (user?.error) {
+    if (user && user?.error) {
       if (typeof window !== "undefined") {
         router.push("/login");
       }
